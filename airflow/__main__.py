ea324db7af8eb4312ef4dc8d77c1482f29c9f323
Jarek Potiuk <jarek@potiuk.com>
2024-01-28 19:20:36 +0100

    Update __main__.py

--- airflow/__main__.py
+++ airflow/__main__.py
@@ -17,7 +17,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""Main executable module."""
+"""Main executable module. """
 from __future__ import annotations
 
 import os
